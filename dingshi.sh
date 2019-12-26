232fbde0c9596b75c00385d854caeeb43acd0d76
as1543100166 <1915450163@qq.com>
2019-12-26 22:39:40 +0800

    Create dingshi.sh

---
+++ dingshi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+myscript=resume_backup.sh
+filePath=$(cd "$(dirname "$0")";pwd)
+echo $filePath
+crontab -l > cron
+number=`grep -n "$myscript" cron | cut -d ":" -f 1`
+aa=$number
+echo $aa
+if [ -z $aa ];
+then
+echo "该任务不存在，将添加"
+echo '30 1 * * * docker restart $(docker ps -a | awk '{ print $1}' | tail -n +2)' >> cron
+crontab cron
+rm -f cron
+else
+echo "该任务已经存在，将会先删除再添加"
+sed -i '/'$myscript'/d' cron
+echo '30 1 * * * docker restart $(docker ps -a | awk '{ print $1}' | tail -n +2)' >> cron
+crontab cron
+rm -f cron
+fi
