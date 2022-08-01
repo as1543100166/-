f63e71b8eb8b7c11665c2c195a4cd628db00ce85
apdins <9900535+as1543100166@users.noreply.github.com>
2022-08-01 22:16:12 +0800

    Add files via upload

--- neihe.sh
+++ neihe.sh
@@ -211,8 +211,9 @@ EOF
     sed -i '/required pam_limits.so/d' /etc/pam.d/common-session
     echo "session required pam_limits.so" >>/etc/pam.d/common-session
   fi
-	read -p "需要重启VPS后，才能生效系统优化配置，是否现在重启 ? [Y/n] :" yn
-	[ -z "${yn}" ] && yn="y"
-	if [[ $yn == [Yy] ]]; then
-		echo -e "${Info} VPS 重启中..."
-		reboot
+  read -p "需要重启VPS后，才能生效系统优化配置，是否现在重启 ? [Y/n] :" yn
+  [ -z "${yn}" ] && yn="y"
+  if [[ $yn == [Yy] ]]; then
+  	echo -e "${Info} VPS 重启中..."
+  	reboot
+  fi
