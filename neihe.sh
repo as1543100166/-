095938ddd37b6308ab5f54d75ef95648fa522650
apdins <9900535+as1543100166@users.noreply.github.com>
2022-08-01 22:16:57 +0800

    Add files via upload

--- neihe.sh
+++ neihe.sh
@@ -214,6 +214,6 @@ EOF
   read -p "需要重启VPS后，才能生效系统优化配置，是否现在重启 ? [Y/n] :" yn
   [ -z "${yn}" ] && yn="y"
   if [[ $yn == [Yy] ]]; then
-  	echo -e "${Info} VPS 重启中..."
-  	reboot
+    echo -e "${Info} VPS 重启中..."
+    reboot
   fi
